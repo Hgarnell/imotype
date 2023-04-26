4eeaa10a931515055f7f2ff976ad9faf909c2df3
Hgarnell <hannah@whiterabbit.com>
2023-04-26 22:22:08 +1200

    Addded shadowing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-const typingText = document.querySelector(".typing-text");
+const typingText = document.querySelector(".typing-text p");
+inpField = document.querySelector(".wrapper .input-field");
+let charIndex = 0;
 
 function randomParagraph(){
-    console.log(paragraphs[1]);
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag =`<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    } );
+    document.addEventListener("keydown",() => inpField.focus());
+    typingText.addEventListener("click",() => inpField.focus());
+
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+
+    if(characters[charIndex].innerText == typedChar)
+    {
+        characters[charIndex].classList.add("correct");
+
+    }else{
+        characters[charIndex].classList.add("incorrect");
+
+    }
+    charIndex++;
+    characters[charIndex].classList.add("active");
+
 }
 randomParagraph();
+inpField.addEventListener("input",initTyping);
