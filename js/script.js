957a55e93e83e8264a16ee5fc315e30f28552ec5
Hgarnell <hannah@whiterabbit.com>
2023-04-28 09:41:04 +1200

    added timer and mistakes

--- js/script.js
+++ js/script.js
@@ -1,35 +1,61 @@
 const typingText = document.querySelector(".typing-text p");
 inpField = document.querySelector(".wrapper .input-field");
-let charIndex = 0;
+mistakeTag = document.querySelector(".mistake span");
+timeTag = document.querySelector(".time span b");
+let  timer,
+  maxTime = 60,
+  timeLeft = maxTime,
+  charIndex = mistakes = isTyping = 0;
 
-function randomParagraph(){
-    let randIndex = Math.floor(Math.random() * paragraphs.length);
+function randomParagraph() {
+  let randIndex = Math.floor(Math.random() * paragraphs.length);
 
-    paragraphs[randIndex].split("").forEach(span => {
-        let spanTag =`<span>${span}</span>`;
-        typingText.innerHTML += spanTag;
-    } );
-    document.addEventListener("keydown",() => inpField.focus());
-    typingText.addEventListener("click",() => inpField.focus());
-
+  paragraphs[randIndex].split("").forEach((span) => {
+    let spanTag = `<span>${span}</span>`;
+    typingText.innerHTML += spanTag;
+  });
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
 }
 
 function initTyping() {
-    const characters = typingText.querySelectorAll("span");
-    let typedChar = inpField.value.split("")[charIndex];
-
-    if(characters[charIndex].innerText == typedChar)
-    {
-        characters[charIndex].classList.add("correct");
-
-    }else{
-        characters[charIndex].classList.add("incorrect");
-
+  const characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if(!isTyping)
+  {
+    timer = setInterval(initTimer,1000);
+    isTyping= true;
+  }
+  if (typedChar == null) {
+    charIndex--;
+    if (characters[charIndex].classList.contains("incorrect")) {
+      mistakes--;
     }
-    charIndex++;
-    characters.forEach(span =>span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
+    characters[charIndex].classList.remove("correct", "incorrect");
+  } else {
+    if (characters[charIndex].innerText == typedChar) {
+      characters[charIndex].classList.add("correct");
+    } else {
+      mistakes++;
+      characters[charIndex].classList.add("incorrect");
+    }
 
+    charIndex++;
+  }
+  characters.forEach((span) => span.classList.remove("active"));
+  characters[charIndex].classList.add("active");
+
+  mistakeTag.innerText = mistakes;
+}
+
+function initTimer(){
+    if(timeLeft>0)
+    {
+        timeLeft--;
+        timeTag.innerText= timeLeft;
+    } else{
+        clearInterval(timer);
+    }
 }
 randomParagraph();
-inpField.addEventListener("input",initTyping);
+inpField.addEventListener("input", initTyping);
